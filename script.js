0baa6edb69ebcfa4145712057475e4e814a91ef9
carlosthodoro <164918114+carlosthodoro@users.noreply.github.com>
2025-07-30 13:37:14 -0300

    g

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function inserirNome(){
+let nomeUsuario = prompt("Qual oseu nome");
     let elemento = document.querySelector("#nome-usuario");
     console.log(elemento);
     elemento.textContent = nomeUsuario;
